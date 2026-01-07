979ea6351704778fd9388fbde3558cfde3404fc0
cqq <leo.q.chen@hotmail.com>
2026-01-07 16:48:06 +0800

    code refine

--- iter_actions.py
+++ iter_actions.py
@@ -9,56 +9,54 @@ if 'renderdoc' not in sys.modules and '_renderdoc' not in sys.modules:
 # Alias renderdoc for legibility
 rd = renderdoc
 
-# Define a recursive function for iterating over actions
-def iterAction(d, indent = ''):
-	# Print this action
-	print('%s%d: %s' % (indent, d.eventId, d.GetName(controller.GetStructuredFile())))
-
-	# Iterate over the action's children
-	for d in d.children:
-		iterAction(d, indent + '    ')
-
 def sampleCode(controller):
-	# Iterate over all of the root actions
-	for d in controller.GetRootActions():
-		iterAction(d)
+	drawcall_count = 0
+	dispatch_count = 0
+	ShaderModule_count = 0
 
-	# Start iterating from the first real action as a child of markers
 	action = controller.GetRootActions()[0]
-
 	while len(action.children) > 0:
 		action = action.children[0]
 
-	# Counter for which pass we're in
-	passnum = 0
-	# Counter for how many actions are in the pass
-	passcontents = 0
-	# Whether we've started seeing actions in the pass - i.e. we're past any
-	# starting clear calls that may be batched together
-	inpass = False
-
-	print("Pass #0 starts with %d: %s" % (action.eventId, action.GetName(controller.GetStructuredFile())))
-
 	while action != None:
-		# When we encounter a clear
-		if action.flags & rd.ActionFlags.Clear:
-			if inpass:
-				print("Pass #%d contained %d actions" % (passnum, passcontents))
-				passnum += 1
-				print("Pass #%d starts with %d: %s" % (passnum, action.eventId, action.GetName(controller.GetStructuredFile())))
-				passcontents = 0
-				inpass = False
-		else:
-			passcontents += 1
-			inpass = True
+		action_is_draw_or_dispatch = False
+
+		if action.flags & rd.ActionFlags.Drawcall:
+			drawcall_count += 1
+			action_is_draw_or_dispatch = True
+
+		if action.flags & rd.ActionFlags.Dispatch:
+			dispatch_count += 1
+			action_is_draw_or_dispatch = True
+
+		if action_is_draw_or_dispatch:
+			controller.SetFrameEvent(action.eventId,False)
+			pipeState = controller.GetPipelineState()
+
+			vertexShader = pipeState.GetShader(rd.ShaderStage.Vertex)
+			geometryShader = pipeState.GetShader(rd.ShaderStage.Geometry)
+			fragmentShader = pipeState.GetShader(rd.ShaderStage.Fragment)
+			computeShader = pipeState.GetShader(rd.ShaderStage.Compute)
+
+			print('%d: %s, shader(vertex=%d, geometry=%d, fragment=%d, compute=%d)' % (action.eventId, action.GetName(controller.GetStructuredFile()), vertexShader, geometryShader, fragmentShader, computeShader))
 
 		# Advance to the next action
 		action = action.next
 		if action is None:
 			break
 
-	if inpass:
-		print("Pass #%d contained %d actions" % (passnum, passcontents))
+	print('drawcall_count: %d' % drawcall_count)
+	print('dispatch_count: %d' % dispatch_count)
+	print("")
+
+	for res in controller.GetResources():
+		if (res.type == rd.ResourceType.Shader):
+			ShaderModule_count += 1
+			print('%s, %s, parent %s, derived %s' % (res.name, res.type.name, res.parentResources, res.derivedResources))
+
+	print('ShaderModule_count: %d' % ShaderModule_count)
+
+	sys.exit(0)
 
 def loadCapture(filename):
 	# Open a capture file handle
@@ -81,8 +79,11 @@ def loadCapture(filename):
 	if result != rd.ResultCode.Succeeded:
 		raise RuntimeError("Couldn't initialise replay: " + str(result))
 
-	return cap,controller
+	return (cap, controller)
 
+#------------------------
+#  main
+#------------------------
 if 'pyrenderdoc' in globals():
 	pyrenderdoc.Replay().BlockInvoke(sampleCode)
 else:
