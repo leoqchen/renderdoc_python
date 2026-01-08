de16e6d4ae753b7a92ff8e61dbe3e4ed70f166df
cqq <leo.q.chen@hotmail.com>
2026-01-08 18:58:31 +0800

    add iter_resources_shader.py

---
+++ iter_resources_shader.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import os
+import sys
+import renderdoc as rd
+
+def loadCapture(filename):
+    # Open a capture file handle
+    cap = rd.OpenCaptureFile()
+
+    # Open a particular file - see also OpenBuffer to load from memory
+    result = cap.OpenFile(filename, '', None)
+
+    # Make sure the file opened successfully
+    if result != rd.ResultCode.Succeeded:
+        raise RuntimeError("Couldn't open file: " + str(result))
+
+    # Make sure we can replay
+    if not cap.LocalReplaySupport():
+        raise RuntimeError("Capture cannot be replayed")
+
+    # Initialise the replay
+    result,controller = cap.OpenCapture(rd.ReplayOptions(), None)
+
+    if result != rd.ResultCode.Succeeded:
+        raise RuntimeError("Couldn't initialise replay: " + str(result))
+
+    return cap,controller
+
+#------------------------
+#  main
+#------------------------
+if 'pyrenderdoc' in globals():
+    pyrenderdoc.Replay().BlockInvoke(sampleCode)
+else:
+    rd.InitialiseReplay(rd.GlobalEnvironment(), [])
+
+    if len(sys.argv) <= 1:
+        print('Usage: python3 {} filename.rdc'.format(sys.argv[0]))
+        sys.exit(0)
+
+    cap,controller = loadCapture(sys.argv[1])
+
+    # Now we can use the controller!
+    print("GetRootActions: %d top-level actions" % len(controller.GetRootActions()))
+
+    print("\nGetResources: %d resources" % len(controller.GetResources()))
+    for res in controller.GetResources():
+        print( f"{res.resourceId}, {res.name}, {res.type.name}" )
+
+    print("\nDisassembly Targets:")
+    targets = controller.GetDisassemblyTargets(True)
+    for disasm in targets:
+        print("  - " + disasm)
+    target = targets[0]
+
+    shaderModule_count = 0
+    shaderDir = "shader"
+    os.makedirs(shaderDir, exist_ok=True)
+
+    print("\nShader Modules:")
+    for res in controller.GetResources():
+        if( res.type == rd.ResourceType.Shader ):
+            shaderModule_count += 1
+            shaderRefl = controller.GetShader( res.derivedResources[0], res.resourceId, controller.GetShaderEntryPoints(res.resourceId)[0] )
+            print('%s, %s, parent %s, derived %s, %s <- %s <- %s, %s' %
+                  (res.name, res.type.name, res.parentResources, res.derivedResources,
+                   shaderRefl.encoding.name, shaderRefl.debugInfo.compiler.name, shaderRefl.debugInfo.encoding.name, shaderRefl.stage.name))
+
+            disasm = controller.DisassembleShader(res.derivedResources[0], shaderRefl, target);
+            file = open( "%s/shader_%d.txt" % (shaderDir, res.resourceId), "w")
+            file.write(disasm)
+            file.close()
+    print("Shader Modules count: %d" % shaderModule_count)
+
+    controller.Shutdown()
+    cap.Shutdown()
+
+    rd.ShutdownReplay()
